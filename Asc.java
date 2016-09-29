165540038a87ebfb491f02ea492c91c5213d428a
Aleksandr S Melnik <alexmelnik224@gmail.com>
2016-09-29 13:33:30 -0700

    Add files via upload

---
+++ Asc.java
@@ -0,0 +1,18 @@
+// sace as Asc.java
+// compile javac Asc.java
+class Asc {
+	public static void main (String a[]){
+		// variables
+		int i; //i is a counter
+		char c; // is a charater
+		// print some text
+		System.out.println("integer/tcharacter");
+		
+		//for loop
+		for (i = 32; i < 1000; i++){
+			c = (char)i;
+			if(i % 10 == 0){System.out.println(i+"="+c+" ");
+			
+		}
+	}
+}			
