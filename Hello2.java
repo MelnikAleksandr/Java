09dd3d8602ad074be85df888f8722c13b3fc1b53
Aleksandr S Melnik <alexmelnik224@gmail.com>
2016-10-25 12:32:34 -0700

    Delete Hello2.java

--- Hello2.java
+++
@@ -1,9 +0,0 @@
- // save this file in a folder called java you have create in home
- // Hello2.java
- 
-class Hello2 {
-	public static void main (String a[]){
-		System.out.print( "hello 2 world!" );
-	}
- }
- 
